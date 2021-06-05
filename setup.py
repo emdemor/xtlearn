831a4b1e1b4073dc42ee9dadaaeaa6dd27e0a69b
Eduardo <eduardo.morais@risknow.ai>
2021-06-05 16:53:09 -0300

    imp: update version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setup(
     name="xtlearn",
-    version="0.0.17",
+    version="0.0.18",
     url="https://github.com/emdemor/xtlearn",
     license="MIT License",
     author="Eduardo M. de Morais",
